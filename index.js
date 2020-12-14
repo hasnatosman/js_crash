1b9814f338343fd16e69bf0687499006ef02640e
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-12-14 17:44:09 +0600

    Add files via upload
    just for the basic.

---
+++ index.js
@@ -0,0 +1,29 @@
+// variable.......................
+let name = 'What is you name ?'
+//console.log(name);
+let firstName = 'Hasnat ';
+//console.log(firstName);
+let lastName = 'Osman'
+//console.log(lastName);
+let x = 5;
+x = 7
+//console.log(x);
+const y = 10;
+//console.log(y);
+
+// object...........................
+
+let person ={
+    name : 'Hasnat',
+    age : 25
+};
+person.name = 'HASNAT OSMAN'
+person.age = 26
+document.write('His name is : ',  person.name , ' & ',  'His age is: ',  person.age);
+
+// function...........................
+
+function greet(){
+    document.write('Hasnat') 
+}
+greet()
